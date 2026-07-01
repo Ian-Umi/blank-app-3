e839f951b3c607b05f48cd52776c546ede9a8297
Ian-Umi <skwoqkmw02059@gmail.com>
2026-07-01 16:32:55 +0000

    test app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("수학 숙제장 생성기")
+st.write("앱 시작 성공")
